b111d2c209d31c7d769e3c624d89ac3fdd23dedc
vijay04 <vijay.mayekar04@gmail.com>
2013-05-31 15:31:31 +0530

    check website status using curl and send mail

--- application/controllers/check.php
+++ application/controllers/check.php
@@ -8,32 +8,39 @@ class Check extends CI_Controller {
   }
 
   function index() {
-  	$sites = array(
-  		'http://www.deccanchronicle.com/index.php',
-  		'http://www.khelnama.com/index.php',
-  	);
+  	$this->load->model('sites_model');
+  	$sites = $this->sites_model->get();
+		foreach ($sites as $key => $value) {
+      $handle = curl_init($value['path']);
+      curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+      $response = curl_exec($handle);
+      $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-		foreach ($sites as $value) {
-	  	file_get_contents($value);
-			switch ($http_response_header[0]) {
-				case 'HTTP/1.1 200 OK':
-					$this->load->library('email');
-
-					$this->email->from('vijay.mayekar@focalworks.in', 'Vijay');
-					$this->email->to('amitav.roy@focalworks.in'); 
-
-					$this->email->subject('Site report for ' . $value . ' at ' . date('jS F g:i A', time()));
-					$this->email->message('Site report for ' . $value . ' at ' . date('jS F g:i A', time()));	
-
-					$this->email->send();
-					break;
-				
+			switch ($httpCode) {
+				case 500:
+          $this->sendmail($httpCode, $value);
+          break;
+          
+        case 403:
+        $this->sendmail($httpCode, $value);
+        break;
+          
 				default:
-					# code...
+          if ($httpCode > 499) {
+            $this->sendmail($httpCode, $value);
+          }
 					break;
 			}
 		}
   }
-
-
+  
+  private function sendmail($code, $value) {
+    $this->load->library('email');
+    $this->email->from('vijay.mayekar@focalworks.in', 'Vijay');
+    $this->email->to($value['email']);
+    $this->email->subject('Reporting error' . $code . 'for site ' . $value['sitename'] . ' @ ' . date('jS F g:i A', time()));
+    $this->email->message('Reporting error' . $code . 'for site ' . $value['sitename'] . ' @ ' . date('jS F g:i A', time()));
+    $this->email->send();
+  }
+  
 }
