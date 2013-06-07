c8c7f30db3901b63577d7cac9d1dc48a21c009da
vijay04 <vijay.mayekar04@gmail.com>
2013-06-07 15:36:42 +0530

    change link for site

--- application/views/sites_list.php
+++ application/views/sites_list.php
@@ -5,13 +5,15 @@
       <tr>
         <th>Sites</th>
         <th>Email</th>
+        <th>Link</th>
         <th><a href="#/new"><i class="icon-plus-sign"></i></a></th>
       </tr>
       </thead>
       <tbody>
       <tr ng-repeat="site in sites | filter:search | orderBy:'sitename'">
-        <td><a href="{{site.path}}" target="_blank">{{site.sitename}}</a></td>
+        <td>{{site.sitename}}</td>
         <td>{{site.email}}</td>
+        <td><a href="{{site.path}}" target="_blank">Visit Site</a></td>
         <td>
           <a href="#/edit/{{site.id}}"><i class="icon-pencil"></i></a>
         </td>
